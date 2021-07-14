01baa97a8b0c809bfe997903cde36bd61f35e564
MichalRzavucki <michael.rzheutski@gmail.com>
2021-07-14 16:03:18 +0300

    add responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,51 @@
-$(function(){
+$(function () {
+  /* Menu nav toggle */
+  $('#nav_toggle').on('click', function (event) {
+    event.preventDefault();
 
+    $(this).toggleClass('active');
+    $('#nav').toggleClass('active');
+  });
 
-
-  
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
